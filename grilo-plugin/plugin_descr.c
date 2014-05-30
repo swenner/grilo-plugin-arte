6fc214eac0a4e862e8a37df68e815afffce8c0d3
Simon Wenner <simon@wenner.ch>
2014-05-30 21:12:18 +0200

    removed the grilo-plugin from the master branch

--- grilo-plugin/plugin_descr.c
+++
@@ -1,8 +0,0 @@
-#include <grilo.h>
-
-extern gboolean
-grl_arteplus7_plugin_init (GrlRegistry *registry,
-        GrlPlugin *plugin,
-        GList *configs);
-
-GRL_PLUGIN_REGISTER (grl_arteplus7_plugin_init, NULL, "grl-arteplus7");
